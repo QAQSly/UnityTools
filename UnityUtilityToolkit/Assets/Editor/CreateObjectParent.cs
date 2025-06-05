94685bf2ab916709fdfed59e0329ad8eaff08821
QAQSly <20154672764@qq.com>
2025-06-05 21:09:49 +0800

    删除多余log

--- UnityUtilityToolkit/Assets/Editor/CreateObjectParent.cs
+++ UnityUtilityToolkit/Assets/Editor/CreateObjectParent.cs
@@ -10,7 +10,6 @@ public class CreateObjectParent : EditorWindow
     public static void CreateParent()
     {
         GameObject[] gos = Selection.gameObjects;
-        Debug.Log("===当前选中的物体===");
         if (gos == null || gos.Length == 0)
         {
             Debug.LogError("未选中物体");
