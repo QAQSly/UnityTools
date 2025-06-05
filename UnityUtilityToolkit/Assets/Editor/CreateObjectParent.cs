9da192cc49b45dbe809799543e19fa272e1795b7
QAQSly <20154672764@qq.com>
2025-06-05 21:38:39 +0800

    增加了父对象颜色

--- UnityUtilityToolkit/Assets/Editor/CreateObjectParent.cs
+++ UnityUtilityToolkit/Assets/Editor/CreateObjectParent.cs
@@ -3,8 +3,28 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+[InitializeOnLoad]
 public class CreateObjectParent : EditorWindow
 {
+    static CreateObjectParent()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += OnParentColorChange;
+    }
+
+    static void OnParentColorChange(int instanceId, Rect selectionRect)
+    {
+        GameObject gameObject = EditorUtility.InstanceIDToObject(instanceId) as GameObject;
+
+        if (gameObject != null && gameObject.name.StartsWith("======"))
+        {
+            Color originalColor = GUI.contentColor;
+            GUI.contentColor = new Color(230f / 255f, 82f / 255f, 124f / 255f);
+            Rect labelRect = new Rect(selectionRect.x + 17, selectionRect.y - 1, selectionRect.width - 20, selectionRect.height);
+            GUIContent content = new GUIContent(gameObject.name);
+            EditorGUI.LabelField(labelRect, content);
+            GUI.contentColor = originalColor;
+        }
+    }
     // Start is called before the first frame update
     [MenuItem("SlyTools/创建选中对象父对象")]
     public static void CreateParent()
